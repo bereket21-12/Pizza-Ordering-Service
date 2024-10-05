decf83f29f7f2002a7212c633947f473ab999542
bereket <berekettadelemulu210@gmail.com>
2024-10-05 21:52:32 +0300

    add active default

--- actions/userAction.ts
+++ actions/userAction.ts
@@ -46,6 +46,7 @@ export async function handleSignUp(previousState: any, formData: FormData) {
     const newUser = await createUser({
       ...userData,
       email,
+      Active: true,
       password: hashedPassword,
       roles: {
         create: {
